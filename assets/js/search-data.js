48d676c01d97a890410c86cd5cc79d929634a39b
SarahiRB <104027893+SarahiRB@users.noreply.github.com>
2025-01-03 16:35:01 +0000

    Deploying to gh-pages from @ SarahiRB/sarahirb.github.io@334b98f8a67363b217441a9d3424175ae424d45c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-about",
+    title: "about",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -52,8 +52,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-example-post",
-          title: 'example post',
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
